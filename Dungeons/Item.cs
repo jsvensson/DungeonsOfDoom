611dc1b721fb72d073ba954deb6027abd99ef2b6
Johan Svensson <johan@atomicplayboy.net>
2015-11-05 12:59:58 +0100

    Basic Item class

---
+++ Dungeons/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeons
+{
+    class Item
+    {
+        public Item(string name, int weight, char symbol)
+        {
+            Name = name;
+            Weight = weight;
+            Symbol = symbol;
+        }
+
+        public string Name { get; private set; }
+        public int Weight { get; private set; }
+        public char Symbol { get; private set; }
+    }
+}
