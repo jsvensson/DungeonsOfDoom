ca8b317d85450d42f17e84a43f708b382c56b157
Johan Svensson <johan@atomicplayboy.net>
2015-11-06 14:46:14 +0100

    Do the monster mash!

---
+++ Dungeons/Monster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeons
+{
+    class Monster : Creature
+    {
+        public Monster(string name, int health, int attackValue) : base(name, health, attackValue)
+        {
+
+        }
+    }
+}
---
+++ Dungeons/Troll.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeons
+{
+    class Troll : Monster
+    {
+        public Troll(string name, int health, int attackValue) : base(name, health, attackValue)
+        {
+            Symbol = 'T';
+        }
+    }
+}
