ea06a83bfc399c233cc12876bab628656c8ec717
Johan Svensson <johan@atomicplayboy.net>
2015-11-13 02:08:16 +0100

    Add scaffold for Ghost

---
+++ Dungeons/GameEntities/Creatures/Ghost.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeons
+{
+    class Ghost : Monster
+    {
+        public Ghost(string name, int health, int attackValue) :
+            base(name, health, attackValue, '@', ConsoleColor.DarkGray)
+        {
+        }
+
+        public override int Fight(Creature opponent)
+        {
+            if (opponent.Health >= Health * 2)
+            {
+                Health = 0;
+            }
+            else
+            {
+                opponent.Health -= AttackValue;
+            }
+
+            return opponent.Health;
+        }
+    }
+}
