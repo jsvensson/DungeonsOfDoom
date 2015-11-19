7c1341dd9c21ae6dc05249d1d200d603aca7e1a7
Johan Svensson <johan@atomicplayboy.net>
2015-11-19 23:35:38 +0100

    Blitter scaffold

---
+++ Dungeons/Blitter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeons
+{
+    static class Blitter
+    {
+        static List<Blixel> blixels = new List<Blixel>();
+
+        public static void Draw()
+        {
+
+        }
+
+        public static void Add(Blixel blixel)
+        {
+            blixels.Add(blixel);
+        }
+
+        static void Clear()
+        {
+            blixels.Clear();
+        }
+
+    }
+}
