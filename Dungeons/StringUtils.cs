395df495c9e814dbdbbd39443aaec2fc44e83ec7
Johan Svensson <johan@atomicplayboy.net>
2015-11-20 14:54:39 +0100

    String stuff

---
+++ Dungeons/StringUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Dungeons
+{
+    static class StringUtils
+    {
+        static public void WriteCenteredLine(string value)
+        {
+            int width = Console.WindowWidth;
+            string padding = new string(' ', width / 2);
+            Console.WriteLine(padding + value);
+        }
+
+        static public string ToUpperFirst(string value)
+        {
+            char[] letters = value.ToCharArray();
+            letters[0] = letters[0].ToString().ToUpper()[0];
+            return new string(letters);
+        }
+    }
+}
