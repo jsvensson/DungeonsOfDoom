713d02bffd0f4959821862bcdedddd77e187fe50
Johan Svensson <johan@atomicplayboy.net>
2015-11-28 22:47:56 +0100

    Return correct tile symbol

--- Dungeons/GameEntities/Tile.cs
+++ Dungeons/GameEntities/Tile.cs
@@ -4,6 +4,7 @@ namespace Dungeons
 {
     abstract class Tile : GameEntity
     {
+        private char symbol;
         public Tile(char symbol, ConsoleColor color) :
             base(symbol, color)
         {
@@ -20,6 +21,16 @@ namespace Dungeons
         {
             get { return Monster != null; }
         }
+        public override char Symbol
+        {
+            get
+            {
+                // Return correct symbol for the tile
+                return GetMapSymbol();
+            }
+            set { symbol = value; }
+
+        }
 
         public Item Item { get; set; }
         public Monster Monster { get; set; }
@@ -27,6 +38,14 @@ namespace Dungeons
         public bool IsNotWalkable
         {
             get { return !IsWalkable; }
+        }
+
+        private char GetMapSymbol()
+        {
+            if (HasItems)
+                return Item.Symbol;
+            else
+                return symbol;
         }
     }
 }
